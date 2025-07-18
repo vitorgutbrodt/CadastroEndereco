bd46ae47f1778e923f2607056cd34722210cc2f8
Vitor <vitor.gutbrodt@gmail.com>
2025-07-17 21:57:21 -0300

    versão final

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ document.getElementById("cep").addEventListener("blur", (evento) => {
     const elemento = evento.target;
     const cepInformado = elemento.value;
 
+    // verificando se já há um CEP salvo no localStorage
+    const dadosSalvos = localStorage.getItem(cepInformado);
+
+    if (dadosSalvos) {
+        console.log("CEP já salvo no localStorage");
+        const dados = JSON.parse(dadosSalvos);
+        console.log(dados);
+    } else {
+        console.log("Ainda não há CEP salvo!")
+    }
+
     // verificando se o CEP informado é válido
     if(!(cepInformado.length === 8))
     return;
@@ -18,16 +29,32 @@ document.getElementById("cep").addEventListener("blur", (evento) => {
                 document.getElementById("cidade").value = data.localidade;
                 document.getElementById("bairro").value = data.bairro;
                 document.getElementById("estado").value = data.uf;
+                localStorage.setItem(cepInformado, JSON.stringify(data)); // salvando os dados no localStorage
+                localStorage.setItem("ultimoCEP", cepInformado) // salvando último CEP para manter os dados na tela ao recarregar
             }else{
                 alert("CEP não encontrado.")
             }
 
         })
         // configurando catch para caso de erro na busca do fetch
-        .catch(error => console.error("Erro ao buscar o CEP", error));
-
-        
+    .catch(error => console.error("Erro ao buscar o CEP", error));
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ultimoCEP = localStorage.getItem("ultimoCEP");
+
+    if (ultimoCEP) {
+        const dadosSalvos = localStorage.getItem(ultimoCEP);
+        if (dadosSalvos) {
+            const dados = JSON.parse(dadosSalvos);
+            document.getElementById("cep").value = ultimoCEP;
+            document.getElementById("logradouro").value = dados.logradouro;
+            document.getElementById("cidade").value = dados.localidade;
+            document.getElementById("bairro").value = dados.bairro;
+            document.getElementById("estado").value = dados.uf;
+        }
+    }
+});
 
 
     
